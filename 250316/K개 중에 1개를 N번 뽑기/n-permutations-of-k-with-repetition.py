57641ad6579bf64d2d4db6012a34a693e9380c6b
wikBurn <93732529+wikBurn@users.noreply.github.com>
2025-03-15 22:01:33 +0100

    Title: K개 중에 1개를 N번 뽑기, Time: 225ms, Memory: 27MB, Status: Passed - CodeTree

--- 250316/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
+++ 250316/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,4 +1,24 @@
 K, N = map(int, input().split())
 
 # Please write your code here.
-print()
+answer = []
+
+
+def print_answer():
+    for element in answer:    
+        print(element, end=' ')
+    print()
+
+def choose_num(curr_number):
+    #Termination
+    if curr_number == N + 1:
+        print_answer()
+        return
+    
+    for i in range(1, K + 1):
+        answer.append(i)
+        choose_num(curr_number + 1)
+        answer.pop()
+    return
+
+choose_num(1)
