30bf6b5cf33552c5582e386c6ef292d9ca3478d8
wikBurn <93732529+wikBurn@users.noreply.github.com>
2025-05-11 19:20:51 +0200

    Title: 연속 부분 합의 최댓값 구하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250512/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -0,0 +1,27 @@
+import sys
+
+INT_MIN =  -sys.maxsize
+n = int(input())
+a = [0] + list(map(int, input().split()))
+# dp[i]: When the position of the last element of the selected contiguous subsequence is i,
+#        the maximum sum obtainable
+dp = [
+    0
+    for _ in range(n + 1)
+]
+print(dp)
+
+def initialize():
+    for i in range(1, n + 1):
+        dp[i] = INT_MIN
+    dp[1] = a[1]
+
+initialize()
+print(dp)
+
+for i in range (2, n+1):
+    dp[i] = max(dp[i - 1] + a[i], a[i])
+print(dp)
+
+ans = max(dp[1:n + 1])
+print(ans)
